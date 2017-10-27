e98c6ee5abd7de64e41bbe23d41d6c0a0631410f
Kurt W <kurt.weston97@gmail.com>
2017-10-28 12:07:39 +1300

    Added files

---
+++ ReadWriteThread.java
@@ -0,0 +1,29 @@
+package Single;
+import java.io.*;
+import java.util.*;
+
+public class ReadWriteThread extends Thread{
+    
+    private BufferedReader input;
+    private PrintWriter output;
+    
+    public ReadWriteThread(InputStream input, OutputStream output){
+        this.input = new BufferedReader(new InputStreamReader(input));
+        this.output = new PrintWriter(output, true);
+    }
+    
+    public void run(){
+        try {
+            String line;
+            while((line = input.readLine()) != null){
+                output.println(line);
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+    
+    
+    
+    
+}
